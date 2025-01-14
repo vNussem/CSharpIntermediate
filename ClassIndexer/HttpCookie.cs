3173aa6c83824bfbcb295dd8d60ef8a5cdec7ec9
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-14 21:33:58 +0200

    class indexer

---
+++ ClassIndexer/HttpCookie.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ClassIndexer;
+
+public class HttpCookie
+{
+    private readonly Dictionary<string, string> _dictionary;
+
+    public HttpCookie()
+    {
+        _dictionary = new Dictionary<string, string>();
+    }
+
+    public string this[string key]
+    {
+        get { return _dictionary[key]; }
+        set { _dictionary[key] = value; }
+    }
+}
---
+++ ClassIndexer/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Mosh";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
