71507a6a338fba744f194a027ddc8aa070e6fefd
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-14 21:13:25 +0200

    class properties

---
+++ ClassProperty/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1));
+
+            Console.WriteLine(person.Age);
+        }
+    }
+}
--- ClassesAccessModifiers/Person.cs
+++ ClassesAccessModifiers/Person.cs
@@ -15,4 +15,5 @@ public class Person
     {
         return _birthdate;
     }
+
 }
