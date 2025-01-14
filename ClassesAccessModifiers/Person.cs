7cff0a86c64af42397c9c182e0ec91a4b2dc04da
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-14 11:47:18 +0200

    access modifiers

---
+++ ClassesAccessModifiers/Person.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ClassesAccessModifiers;
+
+public class Person
+{
+    private DateTime _birthdate;
+
+    public void SetBirthdate(DateTime birthdate)
+    {
+        _birthdate = birthdate;
+    }
+
+    public DateTime GetBirthdate()
+    {
+        return _birthdate;
+    }
+}
---
+++ ClassesAccessModifiers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassesAccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            // person._birthdate = new DateTime(1982, 1, 1);
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
